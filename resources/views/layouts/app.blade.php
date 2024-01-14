858e200b12d75bf2ec0d776fc866a4cdf16c4ebf
paroca <paroca@nexura.com>
2024-01-14 00:31:00 -0500

    login

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -53,7 +53,18 @@
 
                 <!-- checkea si estoy logeado -->
                 @if (auth()->check())
-                    <p class="text-xl">Bienvenido@ <b>{{ auth()->user()->name }}</b></p>
+                <div class="d-flex align-items-center">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-person-fill" viewBox="0 0 16 16">
+                        <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6"/>
+                    </svg>
+                    <!-- Agregando espacio utilizando un elemento div con clase 'ms-2' -->
+                    <div class="ms-2"></div>
+                    <p class="text-xl m-0">
+                        <b>{{ auth()->user()->name }}</b>
+                    </p>
+                </div>
+
+
                     <span class="mx-2"></span>
                     <a href="{{ route('login.destroy') }}" class="btn btn-danger">Cerrar sesión</a>
                 @else
